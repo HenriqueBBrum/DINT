33915926e2d315541d7f3f28131a81243ee9e703
HenriqueBBrum <hbrum499@gmail.com>
2022-08-10 23:13:24 +0000

    Finished fixing graphs scripts

--- testing/python_scripts/rmse_graph_plotter.py
+++ testing/python_scripts/rmse_graph_plotter.py
@@ -6,6 +6,11 @@ import argparse
 import os
 import numpy as np
 from collections import OrderedDict
+import glob
+
+
+metric_unit = {'b': 1, 'k':1000, 'm':1000000, 'g':1000000000}
+
 
 def add_value_labels(ax, spacing=4, y_spacing=0, color='gray'):
     # For each bar: Place a label
@@ -40,79 +45,140 @@ def add_value_labels(ax, spacing=4, y_spacing=0, color='gray'):
             color=color)                      # Vertically align label differently for
                                         # positive and negative values.
 
+# pos = postion in data to be used, mult is the scale factor
+def crete_bar_graph_rects(data, pos, mult):
+    sw_type_count = {}
+    rects = {}
+    labels = []
+    for k, v in data.items():
+        sw_type, push_time = k.split('_')
 
-def plot_rmse_graph(args, data_dict):
-    if len(data_dict) <= 0:
+        if sw_type not in sw_type_count:
+            sw_type_count[sw_type] = 1
+
+        labels.append(push_time)
+        if sw_type not in rects:
+            rects[sw_type] = [(v[pos]/v[0])*mult]
+        else:
+            rects[sw_type].append((v[pos]/v[0])*mult)
+            sw_type_count[sw_type] = sw_type_count[sw_type]+1
+
+    return labels, rects, sw_type_count
+
+
+def plot_bar_graph(filepath, title, ylabel, color, ticks_freq, labels, rects, sw_type_count):
+    fig, ax = plt.subplots()
+    labels = list(dict.fromkeys(labels))
+
+    sw_type_count_x_map = {}
+    for k, v in sw_type_count.items():
+        sw_type_count_x_map[k] = np.arange(1 if v == 0 else v)
+
+    width=0.20
+    ct = 0
+    multi = -1
+    pos = 0
+    for k, v in rects.items():
+        x = sw_type_count_x_map[k]
+        r = ax.bar(x-width*multi*pos, v, width, label=k.capitalize(), hatch='\\', align='edge')
+       
+        if ct%2 == 0:
+            pos = pos +1
+        else:
+            multi = multi*-1
+
+        ct = ct +1
+
+
+    add_value_labels(ax, y_spacing=-2, color='orange')
+
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    
+
+    max_ = 0
+    for rect in rects.values():
+        local_max = max(rect)
+        if(local_max > max_): max_ = local_max
+    
+    ax.set_ylim([0, max_+0.5*max_])
+    start, end = ax.get_ylim()
+    ax.yaxis.set_ticks(np.arange(start, end, ticks_freq))
+
+    ax.set_xticklabels(labels)
+    ax.set_xticks(x)
+    ax.set_xlabel('Telemetry push time (s)')
+
+    h1, l1 = ax.get_legend_handles_labels()
+    ax.legend(h1, l1, loc='upper center')
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    fig.savefig(filepath)
+
+# Review error with unit change. Problem is probably an excess of ticks or labels
+def plot_graphs(output_folder, traffic_type, sw_id, total_time, data, unit):
+    if len(data) <= 0:
         return
 
-    x = np.arange(len(data_dict))
-    width=0.15
-
-    fig, ax1 = plt.subplots()
-
-    color = 'tab:orange'
-    ax1.set_ylabel('RMSE (%)')
-    ax1.tick_params(axis='y', labelcolor=color)
-    rects1 = ax1.bar(x-width, [(value[1]/value[0])*100 for value in data_dict.values()], width*2, label='Measurement Error', color=color, hatch='\\')
-    add_value_labels(ax1, y_spacing=-2, color='orange')
-
-    ax2 = ax1.twinx()
-    color = 'tab:green'
-    ax2.set_ylabel('Byte overhead ('+args.traffic_shape.split("_")[-1]+' secs)')
-    ax2.tick_params(axis='y', labelcolor=color)
-    ax2.set_ylim([0, 3200])
-    rects2 = ax2.bar(x + width, [value[2]/value[0] for value in data_dict.values()], width*2, label='Telemetry Overhead', color=color, hatch="/")
-    add_value_labels(ax2, y_spacing=6, color='green')
-
-    ax1.set_title('Telemetry overhead and measurement error')
-    ax1.set_xticks(x)
-    ax1.set_ylim([0, 26])
-    start, end = ax1.get_ylim()
-    ax1.yaxis.set_ticks(np.arange(start, end,5))
-
-    ax1.set_xticklabels(data_dict.keys())
-    ax1.set_xlabel('Telemetry push time (s)')
-
-    #print(labels)
-    h1, l1 = ax1.get_legend_handles_labels()
-    h2, l2 = ax2.get_legend_handles_labels()
-    ax2.legend(h1+h2, l1+l2, loc='upper center')
-
-    #fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    rmse_title = 'Measurement Error - '+'SW'+sw_id+' | '+total_time+'s'
+    rmse_filepath = output_folder+traffic_type+'_RMSE_'+sw_id+'_'+total_time.split('.')[0]+'s.png'
+    plot_bar_graph(rmse_filepath, rmse_title, 'RMSE (%)', 'tab:orange', 5, *crete_bar_graph_rects(data, 1, 100))
 
 
+    byte_cnt_title = 'Telemetry Overhead - '+'SW'+sw_id+' | '+total_time+'s'
+    byte_cnt_filepath = output_folder+traffic_type+'s_Tel_Overhead_'+sw_id+'_'+total_time.split('.')[0]+'s.png'
+    plot_bar_graph(byte_cnt_filepath, byte_cnt_title, 'Bytes ('+unit.upper()+'B)', 'tab:green', 100, *crete_bar_graph_rects(data, 2, metric_unit[unit]))
 
-    fig.savefig('graphs/'+args.traffic_shape+'_RMSE_X_Byte.png', dpi=270)
-    plt.show()
+  
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--file_folder', type=str, help="Folder with input files", required=True)
+    parser.add_argument('-g', '--graphs_output_folder', type=str, help="Folder for output files", required=True)
+    parser.add_argument('-u', '--unit', type=str, help = "Metric Unit (b, k, m, g)", required=False, default="b")
+
+
+    return vars(parser.parse_args())
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file_name', type=str, help = "Csv file with data")
-    parser.add_argument('traffic_shape', type=str, help = "Traffic type")
+    args = parse_args()
 
+    rmse_and_byte_cnt_files = glob.glob(args['file_folder']+"*.csv")
+    for f in rmse_and_byte_cnt_files:
+        traffic_type = f.split("/")[-1].split(".")[0]
 
-    args = parser.parse_args()
+        my_dict = OrderedDict()
+        graph_dict = OrderedDict()
 
-    my_dict = OrderedDict()
+        with open(f) as csvfile:
+            data = csv.DictReader(csvfile, delimiter=',')
+            for row in data:
+                f_key = row['sw_id']+"_"+row['experiment_time']
+                s_key = row['sw_type']+"_"+row['min_telemetry_push_time']
+                if (f_key+s_key) in my_dict:
+                    count, previous_rmse, previous_byte_count, previous_experiment_time = my_dict[(f_key+s_key)]
+                    updated_value = (float(count+1), previous_rmse+float(row['rmse']), previous_byte_count+float(row['telemetry_byte_count']),
+                                previous_experiment_time+float(row['experiment_time']))
 
-    with open(args.file_name) as csvfile:
-        data = csv.DictReader(csvfile, delimiter=',')
-        for row in data:
-            if row['min_telemetry_push_time'] in my_dict:
-                count, previous_rmse, previous_byte_count, previous_experiment_time = my_dict[row['min_telemetry_push_time']]
-                my_dict[row['min_telemetry_push_time']] = (float(count+1), previous_rmse+float(row['rmse']), previous_byte_count+float(row['byte_count']),
-                            previous_experiment_time+float(row['experiment_time']))
-            else:
-                my_dict[row['min_telemetry_push_time']] = (1, float(row['rmse']), float(row['byte_count']), float(row['experiment_time']))
-    final_dict = OrderedDict()
-    for i in sorted (my_dict):
-        final_dict[i] = my_dict[i]
+                    my_dict[(f_key+s_key)] = updated_value
+                    graph_dict[f_key][s_key] = updated_value
+                else:
+                    value = (1, float(row['rmse']), float(row['telemetry_byte_count']), float(row['experiment_time']))
+                    my_dict[(f_key+s_key)] = value
+                    if f_key not in graph_dict:
+                        graph_dict[f_key] = {}
+                        graph_dict[f_key][s_key] = value
+                    else:
+                        graph_dict[f_key][s_key] = value
 
-    plot_rmse_graph(args, final_dict)
-
-
+       
+        final_dict = OrderedDict()
+        for k, v in graph_dict.items():
+            print(k, v)
+            sw_id, total_time = k.split('_')
+            plot_graphs(args['graphs_output_folder'], traffic_type, sw_id, total_time, v, args['unit'])
+           
 
 
 if __name__ == '__main__':
