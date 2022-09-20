4c85c6d5075ebb9e79b47392ea5f4934166c81de
HenriqueBBrum <hbrum499@gmail.com>
2022-09-20 12:26:23 +0000

    Added jitter graph for each host

--- testing/run_testing.sh
+++ testing/run_testing.sh
@@ -16,8 +16,8 @@ make P4_SRC=main_sINT.p4 TEST_JSON=$test_files_folder/sINT_tcpreplay.json
 
 cd ../testing/python_scripts
 
-python3 line_graph.py -i $scripts_input_dir/pkts_output/ -j $scripts_input_dir/jitter_input/ -g \
-$scripts_input_dir/graphs_output/ -r $scripts_input_dir/rmse_graphs_input/ -d 60 -m 0.25 -s 2 -u "m" -t "Payless"
+python3 link_util_plot.py -i $scripts_input_dir/pkts_output/ -j $scripts_input_dir/jitter_input/ -g \
+$scripts_input_dir/graphs_output/ -r $scripts_input_dir/graphs_input/ -d 60 -m 0.25 -s 2 -u "m" -t "Payless"
 
-python3 rmse_graph_plotter.py -i $scripts_input_dir/rmse_graphs_input/ -g $scripts_input_dir/graphs_output/ 
+python3 rmse_and_overhead_plot.py -i $scripts_input_dir/graphs_input/ -g $scripts_input_dir/graphs_output/ 
 
