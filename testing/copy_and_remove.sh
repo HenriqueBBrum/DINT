997bb1b144617740179a174f93ee7f170bdfd979
HenriqueBBrum <hbrum499@gmail.com>
2022-09-20 12:26:23 +0000

    Added jitter graph for each host
    Former-commit-id: 00ff202b584bacbd0af9f638fc4ec4b42b21ca16

--- testing/copy_and_remove.sh
+++ testing/copy_and_remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-output_folder=~/Documents/Masters/past_results/round1/results_0.25_3
+output_folder=~/Documents/Masters/past_results/latest/results_0.25_1
 scripts_input_dir=~/Documents/Masters/testing/results
 
 
@@ -17,8 +17,8 @@ cp -r $scripts_input_dir/* $output_folder
 
 sudo rm $scripts_input_dir/graphs_output/*
 sudo rm $scripts_input_dir/pkts_output/*
-sudo rm $scripts_input_dir/rmse_graphs_input/*
+sudo rm $scripts_input_dir/graphs_input/*
 sudo rm $scripts_input_dir/jitter_input/*
 
 
-cp $output_folder/rmse_graphs_input/* $scripts_input_dir/rmse_graphs_input
+cp $output_folder/graphs_input/* $scripts_input_dir/graphs_input
--- testing/python_scripts/link_util_plot.py
+++ testing/python_scripts/link_util_plot.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
-from math import sqrt, fabs
+from math import sqrt, fabs, ceil
 import csv
 import argparse
 import json
@@ -63,36 +63,8 @@ def real_traffic_data(filepath, min_push_time,  unit, experiment_duration):
                 grouped_amt_bytes=int(row['frame.len'])
 
         xy[current_time+min_push_time] = grouped_amt_bytes/(metric_unit[unit]*min_push_time)
-       
-    return xy
-
-
-# Reads real data from csv file to find the amount of bytes transported at the same timestamp that was telemetry reported
-def real_traffic_data_tel(filepath, telemetry_x, unit, experiment_duration):
-    xy = {0: 0}
-    count = 1
-
-    with open(filepath) as csvfile:
-        data = csv.DictReader(csvfile, delimiter=',')
-        for idx, i in enumerate(telemetry_x):
-            if idx == 0:
-                continue
-           
-            grouped_amt_bytes = 0
-            ct = 0
-        
-            for row in data:
-                time = float(row['frame.time_relative'])
-
-                if time > i:
-                    break
-                if time <= i:
-                    grouped_amt_bytes+=int(row['frame.len'])
-                    ct+=1
-
-            xy[float("{:.6f}".format(i))] = float("{:.6f}".format((grouped_amt_bytes/(i - telemetry_x[idx-1]))/metric_unit[unit]))
-        
-    return xy
+    
+    return list(xy.keys()), list(xy.values())
 
 
 # Reads telemetry data from custom txt file to find the amount of bytes transported each second
@@ -131,44 +103,37 @@ def telemetry_traffic_data(telemetry_file, switch_id, unit, experiment_duration)
 
     return x, y, telemetry_byte_count
 
-# Fill 'y' array with values in 'expanded_x' but not in 'x' (original array)
-def my_interpolation(expanded_x, x, y):
+
+
+def find_telemetry_traffic(real_x, telemetry_x, telemetry_y):
     i, j = 0, 0
     expanded_y = []
-    while j<len(x) and i<len(expanded_x):
-        if expanded_x[i] > x[j]:
-            j+=1
-        else:
-            expanded_y.append(y[j])
+    while i <len(real_x) and j<len(telemetry_x)-1:
+        if(real_x[i] < telemetry_x[j+1]):
+            expanded_y.append(telemetry_y[j])
             i+=1
+        else:
+            j+=1
+
+    expanded_y.append(telemetry_y[j])
 
     return expanded_y
 
 
-# Find common values between telemetry and real data and plot link utilization graph
-def plot_line_graph(args, sw_type, real_data, telemetry_data):
-    real_x, real_y = real_data
-    telemetry_x, telemetry_y = telemetry_data
-
-    combined_x = np.sort(np.concatenate([real_x, telemetry_x]))
-
-    tel_expanded_y = my_interpolation(combined_x, telemetry_x, telemetry_y)
-    real_expanded_y = my_interpolation(combined_x, real_x, real_y)
-   
-    real_expanded_y = [x * 8 for x in real_expanded_y]
-    tel_expanded_y = [x * 8 for x in tel_expanded_y]
+# Plot link utilization graph
+def plot_line_graph(args, sw_type, real_x, real_y, telemetry_y):
+    real_y = [x * 8 for x in real_y]
     telemetry_y = [x * 8 for x in telemetry_y]
 
     plot1 = plt.figure(1)
 
-    plt.step(combined_x, real_expanded_y, color="b", label='Real')
-    plt.step(combined_x, tel_expanded_y, color="r", label='Telemetry')
+    plt.step(real_x, real_y, color="b", label='Real')
+    plt.step(real_x, telemetry_y, color='r', label="Telemetry")
 
-    plt.plot(telemetry_x[1:-1], telemetry_y[1:-1], 'o', color='black')
-    plt.plot([telemetry_x[0], telemetry_x[-1]] , [telemetry_y[0], telemetry_y[-1]], '*', color='red');
-    plt.plot([real_x[0], real_x[-1]] , [real_expanded_y[0], real_expanded_y[-1]], '*', color='blue');
+    plt.plot([real_x[0], real_x[-1]] , [telemetry_y[0], telemetry_y[-1]], '*', color='red');
+    plt.plot([real_x[0], real_x[-1]] , [real_y[0], real_y[-1]], '*', color='blue');
 
-    plt.fill_between(combined_x, real_expanded_y, tel_expanded_y, step='pre',  interpolate=True, facecolor='black', alpha=0.2, hatch="X")
+    plt.fill_between(real_x, real_y, telemetry_y, step='pre',  interpolate=True, facecolor='black', alpha=0.2, hatch="X")
 
 
     plt.xlabel("Time(sec)")
@@ -180,10 +145,8 @@ def plot_line_graph(args, sw_type, real_data, telemetry_data):
     plot1.savefig(args['graphs_output_folder']+args['traffic_shape']+'_Real_X_Telemetry_'+sw_type+'_sw'+args['switch_id']+'.png')
     plot1.clf() 
 
-    return real_expanded_y, tel_expanded_y
 
-
-# Calculate jitter of each experiment
+# Calculate jitter of each host of the experiment
 def find_jitter(args, sw_type):
     src_pcapng_files = glob.glob(args['jitter_input_folder']+sw_type+"*.pcapng")
     dest_csv = glob.glob(args['input_file_folder']+sw_type+"_real*.csv")[0]
@@ -191,10 +154,11 @@ def find_jitter(args, sw_type):
     dest_traffic = pd.read_csv(dest_csv)
    
     src_data = {}
-    jitter = 0.0
+    jitter_dict = {}
 
     for src_file in src_pcapng_files:
-        host = src_file[src_file.find('h'):src_file.find('h')+2]
+        filename = src_file.split("/")[-1]
+        host = filename[filename.find('h'):filename.find('h')+2]
         
         paths = src_file.split("/")
         filename = paths[-1].split(".")[0]
@@ -207,28 +171,31 @@ def find_jitter(args, sw_type):
         ip_src = src_traffic.iloc[0]['ip.src']
         sub_dest_traffic = dest_traffic[dest_traffic['ip.src'] == ip_src]
 
+        if(len(sub_dest_traffic['frame.time_epoch'].to_numpy()) != len(src_traffic['frame.time_epoch'].to_numpy())):
+            continue
+
         time_delay = np.subtract(sub_dest_traffic['frame.time_epoch'].to_numpy(), src_traffic['frame.time_epoch'].to_numpy())
         avg_latency = sum(time_delay)/len(time_delay)
 
-        jit = 0.0
+        jitter = 0.0
         iterations = len(time_delay)-1
         for idx in range(0, iterations):
             diff = fabs(time_delay[idx] - time_delay[idx+1])
-            jit+=diff
+            jitter+=diff
 
-        jitter = jitter + (jit/iterations)*ms
+        jitter_dict[k] =  (jitter/iterations)*ms
 
-    return jitter/len(src_pcapng_files)
+    return jitter_dict
 
 
 
 # Saves to a specific file rmse(%), byte count(Bytes) and jitter(ms) information
-def save_rmse_and_telemetry_byte_count(args, sw_type, telemetry_pkts_count, rmse_expanded, rmse_simple, telemetry_byte_count, jitter):
+def save_rmse_and_telemetry_byte_count(args, sw_type, telemetry_pkts_count, rmse, telemetry_byte_count, jitter):
     filepath = args['rmse_output_folder']+args['traffic_shape']+".csv"
     file_exists = os.path.isfile(filepath)
 
     with open(filepath, "a") as csvfile:
-        headers = ['sw_type', 'sw_id', 'min_telemetry_push_time', 'experiment_time', 'packet_count', 'rmse_expanded', 'rmse_simple', 'telemetry_byte_count', 'jitter']
+        headers = ['sw_type', 'sw_id', 'min_telemetry_push_time', 'experiment_time', 'packet_count', 'rmse', 'telemetry_byte_count', 'jitter']
         writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
 
         if not file_exists:
@@ -236,8 +203,8 @@ def save_rmse_and_telemetry_byte_count(args, sw_type, telemetry_pkts_count, rmse
 
         writer.writerow({'sw_type': sw_type, 'sw_id':args['switch_id'], 'min_telemetry_push_time': args['min_telemetry_push_time'], 
                     'experiment_time': args['experiment_duration'], 'packet_count': telemetry_pkts_count, 
-                        'rmse_expanded': rmse_expanded, 'rmse_simple': rmse_simple, 'telemetry_byte_count': telemetry_byte_count,
-                        'jitter': jitter})
+                        'rmse': rmse, 'telemetry_byte_count': telemetry_byte_count, 'jitter': jitter})
+
 
 
 
@@ -255,51 +222,37 @@ def main():
 
    
     telemetry_files = glob.glob(args['input_file_folder']+"*.txt")
-    telemetry_data = {}
     rmse_dict = {}
 
     for f in telemetry_files:
         sw_type = f.split("/")[-1].split(".")[0].split("_")[0]
 
         telemetry_x, telemetry_y, telemetry_byte_count = telemetry_traffic_data(f, args['switch_id'], args['unit'], args['experiment_duration'])
-        
+
         real_traffic_file = glob.glob(args['input_file_folder']+sw_type+"_real*.csv")[0]
-        real_xy = real_traffic_data(real_traffic_file, args['min_telemetry_push_time'], args['unit'], args['experiment_duration'])
+        real_x, real_y = real_traffic_data(real_traffic_file, args['min_telemetry_push_time'], args['unit'], args['experiment_duration'])
         
 
-        if telemetry_x[-1] < list(real_xy.keys())[-1]:
-            telemetry_x.append(list(real_xy.keys())[-1])
+        if telemetry_x[-1] < real_x[-1]:
+            telemetry_x.append(real_x[-1])
             telemetry_y.append(telemetry_y[-1])
 
+        tel_y = find_telemetry_traffic(real_x, telemetry_x, telemetry_y)
 
-        telemetry_data[sw_type] = (telemetry_x, telemetry_y)
-        real_expanded_y, tel_expanded_y= plot_line_graph(args, sw_type, (list(real_xy.keys()), list(real_xy.values())), telemetry_data.get(sw_type))
+        plot_line_graph(args, sw_type, real_x, real_y, tel_y)
 
        
-        print(sw_type, len(real_expanded_y), args['min_telemetry_push_time'], len(telemetry_y))
+        print(sw_type, len(real_y), args['min_telemetry_push_time'], len(tel_y))
 
-        rmse_expanded = sqrt(np.square(np.subtract(real_expanded_y, tel_expanded_y)).mean())
-        rmse_expanded = rmse_expanded/(max(real_expanded_y) - min(real_expanded_y))
-        print("rmse", rmse_expanded)
+        rmse = sqrt(np.square(np.subtract(real_y, tel_y)).mean())
+        rmse = rmse/(max(real_y) - min(real_y))
+        print("rmse", rmse)
+
+        
+        save_rmse_and_telemetry_byte_count(args, sw_type, len(telemetry_x), rmse, telemetry_byte_count, find_jitter(args, sw_type))
+       
 
 
-        ## Second way of finding values. Calculate real traffic in all points
-
-        real_xy_ = real_traffic_data_tel(real_traffic_file, telemetry_x, args['unit'], args['experiment_duration'])
-      
-        merge = {**real_xy_, **real_xy}
-        sorted_merge = dict(sorted(merge.items()))
-        real_x = list(sorted_merge.keys())
-        real_y = list(sorted_merge.values())
-
-        tel_expanded_y = my_interpolation(real_x, telemetry_x, telemetry_y)
-
-        rmse_simple = sqrt(np.square(np.subtract(real_y, tel_expanded_y)).mean())
-        rmse_simple = rmse_simple/(max(real_y) - min(real_y))
-
-        jitter = find_jitter(args, sw_type)
-
-        save_rmse_and_telemetry_byte_count(args, sw_type, len(telemetry_x), rmse_expanded, rmse_simple, telemetry_byte_count, jitter)
 
   
 if __name__ == '__main__':
