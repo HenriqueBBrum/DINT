602e67b2b9231126af95be44ea3d8738b3128e8b
HenriqueBBrum <hbrum499@gmail.com>
2022-11-09 19:53:04 +0000

    Removed unused code, added comments, fixed some code

--- testing/scripts/copy_and_remove.sh
+++ testing/scripts/copy_and_remove.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Copy some of the contents of the testing/results folder and remove them afterwards
+
 output_folder=$1
-jitter=$2
-scripts_input_dir=~/Documents/Masters/testing/results
+scripts_input_dir=~/Documents/DINT/testing/results
 
 
 
 mkdir $output_folder
 
-sudo rm $scripts_input_dir/jitter_input/*.csv
 sudo rm $scripts_input_dir/pkts_output/*.csv
 
 
@@ -18,12 +18,6 @@ cp -r $scripts_input_dir/* $output_folder
 sudo rm $scripts_input_dir/graphs_output/*
 sudo rm $scripts_input_dir/pkts_output/*
 sudo rm $scripts_input_dir/graphs_input/*
-sudo rm $scripts_input_dir/jitter_input/*
 
-
+# Keep the file used in the comparion_plots.py script. When changing the experiment (comparson, alpha or k) delete the file
 cp $output_folder/graphs_input/* $scripts_input_dir/graphs_input
-
-if [ $jitter = "false" ]
-then 
-	sudo rm -r $output_folder/jitter_input
-fi
--- testing/scripts/run_comparison_evaluation.sh
+++ testing/scripts/run_comparison_evaluation.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Run the evaluation that compares DINT with sINT, LINT and a static method
+# This file does not modify the minimum telemetry values in the P4 source files so you need to change them manually in each run
+
+
 echo "Running comparison evaluation"
 echo "Min push time: $1"
 
 
-config_files_folder=~/Documents/Masters/testing/config_files/comparison_evaluation
+config_files_folder=~/Documents/DINT/testing/config_files
 scripts_input_dir=$2
 
 cd ../src 
@@ -23,8 +27,8 @@ done
 cd ../testing/plotting_scripts
 
 
+# Plot line graphs indicating the real link utilization and the one reported by the monitoring algorithm 
 
-python3 link_utilization_plots.py -i $scripts_input_dir/pkts_output/ -j $scripts_input_dir/jitter_input/ -g \
-$scripts_input_dir/graphs_output/ -r $scripts_input_dir/graphs_input/ -d 60 -m "$1" -s 2 -u "m" -t "Payless" --plot_jitter
-
+python3 link_utilization_plots.py -i $scripts_input_dir/pkts_output/ -g \
+$scripts_input_dir/graphs_output/ -r $scripts_input_dir/graphs_input/ -d 60 -m "$1" -s 2 -u "m" -t "Payless"
 
